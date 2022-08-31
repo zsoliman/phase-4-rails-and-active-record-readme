4a2a91be85b2471000a1400341f8582c7d8ed1ee
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-31 09:50:35 -0400

    commit tree 879297cc2b7891674f2e88a46c36f96afd19ebb4

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_142432) do
+ActiveRecord::Schema.define(version: 2022_08_31_134017) do
 
   create_table "cheeses", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.boolean "is_best_seller"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "grade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
